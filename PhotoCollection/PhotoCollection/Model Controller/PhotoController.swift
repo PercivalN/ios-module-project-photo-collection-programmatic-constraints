b8d07efd4fc7d83dfd121eae19dac2af9b4f58d4
Percy <percival.ngan@gmail.com>
2019-10-07 15:43:06 -0500

    Added image and label to cell and implemented their constraints

--- PhotoCollection/PhotoCollection/Model Controller/PhotoController.swift
+++ PhotoCollection/PhotoCollection/Model Controller/PhotoController.swift
@@ -9,25 +9,25 @@
 import Foundation
 
 class PhotoController {
-    
-    func createPhoto(with imageData: Data, title: String) {
-        let photo = Photo(imageData: imageData, title: title)
-        
-        photos.append(photo)
-    }
-    
-    func update(photo: Photo, with imageData: Data, and title: String) {
-        guard let index = photos.firstIndex(of: photo) else { return }
-        
-        var scratch = photo
-        
-        scratch.imageData = imageData
-        scratch.title = title
-        
-        photos.remove(at: index)
-        photos.insert(scratch, at: index)
-    }
-    
-    
-    var photos: [Photo] = []
+
+	// MARK: - Properties
+	var photos: [Photo] = []
+
+	func createPhoto(with imageData: Data, title: String) {
+		let photo = Photo(imageData: imageData, title: title)
+
+		photos.append(photo)
+	}
+
+	func update(photo: Photo, with imageData: Data, and title: String) {
+		guard let index = photos.firstIndex(of: photo) else { return }
+
+		var scratch = photo
+
+		scratch.imageData = imageData
+		scratch.title = title
+
+		photos.remove(at: index)
+		photos.insert(scratch, at: index)
+	}
 }
--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -10,7 +10,63 @@ import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
 
-    var photo: Photo?
+	private var imageView: UIImageView!
+	private var nameLabel: UILabel!
+
+	var photo: Photo? {
+		didSet {
+		}
+	}
+
+	// To programmtically initialize cell
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupSubviews()
+	}
+
+	// The storyboard and xib initializing the cell
+	required init?(coder: NSCoder) {
+		super .init(coder: coder)!
+		setupSubviews()
+	}
+
+	private func setupSubviews() {
+
+		// Create image and setup constraints
+		let imageView = UIImageView()
+		imageView.contentMode = .scaleAspectFit
+
+		addSubview(imageView)
+		imageView.translatesAutoresizingMaskIntoConstraints = false
+
+		NSLayoutConstraint.activate([
+			imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2),
+			imageView.topAnchor.constraint(equalTo: topAnchor, constant: 4),
+			imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2),
+			imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1)
+			])
+
+		self.imageView = imageView
+
+		// Create lable and setup constraints
+		let label = UILabel()
+		label.textAlignment = .center
+
+		addSubview(label)
+		label.translatesAutoresizingMaskIntoConstraints = false
+
+		NSLayoutConstraint.activate([
+			label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2),
+			label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 4),
+			label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2)
+			])
+
+		self.nameLabel = label
+	}
     
-    
+	private func updateViews() {
+		guard let photo = photo else { return }
+		imageView.image = UIImage(data: photo.imageData)
+		nameLabel.text = photo.title
+	}
 }
